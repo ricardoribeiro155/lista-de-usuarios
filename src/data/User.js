8c7659a7bb8e95a5c5e6ba924a2f1db0e01a6bf7
ricardo <ricardoribeiro155@gmail.com>
2025-05-19 12:35:12 -0300

    versão inicial

---
+++ src/data/User.js
@@ -0,0 +1,79 @@
+
+
+export default [
+    {
+        id: 1,
+        name: "jose antonio silva",
+        email: 'jose@hotemail.com',
+        avatarUrl: 'https://cdn.pixabay.com/photo/2016/03/31/19/58/avatar-1295430_1280.png'
+    },
+    {
+        id: 2,
+        name: 'Ana clara pereira',
+        email: 'ana.silva@email.com',
+        avatarUrl: 'https://cdn.pixabay.com/photo/2022/09/19/07/32/android-7464836_1280.jpg'
+    },
+    {
+        id: 3,
+        name: 'carlos alberto',
+        email: 'carlos123@gmail.com',
+        avatarUrl: 'https://cdn.pixabay.com/photo/2016/12/07/21/01/cartoon-1890438_1280.jpg'
+    },
+    {
+        id: 4,
+        name: 'mariana antonieta',
+        email: 'mariana.rosa@outlook.com',
+        avatarUrl: 'https://cdn.pixabay.com/photo/2021/10/29/08/47/woman-6751389_1280.png'
+
+    },
+    {
+        id: 5,
+        name: 'Joao batista',
+        email: 'joao_pereira@yahoo.com',
+        avatarUrl: 'https://cdn.pixabay.com/photo/2021/03/03/10/20/portrait-6064965_1280.jpg'
+
+    },
+    {
+        id: 6,
+        name: 'Fernanda aline vieira',
+        email: 'fernanda98@uol.com.br',
+        avatarUrl: 'https://cdn.pixabay.com/photo/2016/08/28/13/12/secondlife-1625903_1280.jpg'
+
+    },
+    {
+        id: 7,
+        name: ' Roberta malufe',
+        email: 'roberta.lima@bol.com.br',
+        avatarUrl: 'https://cdn.pixabay.com/photo/2021/08/14/07/21/child-6544739_1280.png'
+
+    },{
+        id: 10,
+        name: 'mariana antonieta',
+        email: 'mariana.rosa@outlook.com',
+        avatarUrl: 'https://cdn.pixabay.com/photo/2021/10/29/08/47/woman-6751389_1280.png'
+
+    },
+    {
+        id: 12,
+        name: 'Joao batista',
+        email: 'joao_pereira@yahoo.com',
+        avatarUrl: 'https://cdn.pixabay.com/photo/2021/03/03/10/20/portrait-6064965_1280.jpg'
+
+    },
+    {
+        id: 65,
+        name: 'Fernanda aline vieira',
+        email: 'fernanda98@uol.com.br',
+        avatarUrl: 'https://cdn.pixabay.com/photo/2016/08/28/13/12/secondlife-1625903_1280.jpg'
+
+    },
+    {
+        id: 74,
+        name: ' Roberta malufe',
+        email: 'roberta.lima@bol.com.br',
+        avatarUrl: 'https://cdn.pixabay.com/photo/2021/08/14/07/21/child-6544739_1280.png'
+
+    }
+
+
+];
